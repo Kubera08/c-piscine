a86780b7b5e8607af92c8c66a30e4f2b659aa3d5
Arnaud BEAUDUIN <abeaudui@e3r10p21.clusters.42paris.fr>
2022-07-22 12:59:07 +0200

    wiojdwjd

---
+++ essai.c
@@ -0,0 +1,35 @@
+int ft_atoi(char *str)
+{	
+	int i;
+	int res;
+	int signe;
+	int boite;
+	i = 0;
+	res = 0;
+	signe = 1;
+	boite = 0;
+	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13) || str[i] == '\n')
+		i++;
+	while(str[i] == '-' || str[i] == '+')
+	{
+		boite++;
+		if(str[i] == '-')
+			signe = signe * -1;
+		if(boite == 2)
+			return (0);
+	}
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		res = res  * 10 + str[i] - '0';
+		i++;
+	}
+	return(res * signe);
+}
+
+#include<stdio.h>
+int main(int ac, char **av)
+{
+	ac = ac;
+	printf("%d\n", ft_atoi(av[1]));
+	return 0;
+}
